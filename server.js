79000cf141ed9c2f2e216f297130a9c18d3cec54
jsanchez0112 <edsaja12@gmail.com>
2022-05-19 13:35:30 -0500

    on pace

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 const data = require('./models/pokemon.js')
-
+const smData = [data[0], data[3], data[5], data[10], data[24]]
 app.use((req, res, next) => {
       console.log('working')
       next()
@@ -13,12 +13,24 @@ app.use(express.urlencoded({extended:false}))
 
 app.get('/pokemon' , (req,res) => {
       res.render('index.ejs', {
-            pokemon: data
+            pokemon: smData
       })})
 
+//Adding a new pokemon
 app.get('/pokemon/new', (req,res) => {
       res.render('new.ejs')
 });
+
+app.post('/pokemon' , (req,res) => {
+console.log(req.body)
+data.push(req.body)
+res.redirect('/pokemon')
+})
+
+app.get('/pokemon/:indexOfSmData' , (req,res) => {
+      res.render('show.ejs', {
+      pokemon: smData[req.params.indexOfSmData]
+      })});
 
 app.get('/pokemon/edit' , (req,res) => {
       res.render('edit.ejs')
@@ -32,5 +44,5 @@ app.get('/pokemon/edit' , (req,res) => {
 
 
 app.listen(port , () => {
-      console.log('listening')
+      console.log(smData)
 })
